ab00d37af27f11c4006339a8c4b139f71ce220d7
Darley Soares <53305419+DarleySoares@users.noreply.github.com>
2019-12-14 14:22:44 -0300

    Update cliente_tcp.c

--- cliente_tcp.c
+++ cliente_tcp.c
@@ -20,71 +20,68 @@ void main(int argc, char *argv[])
         exit(1);
     }
 
-    int fd, num_bytes = 0;
+    int fd, num_bytes = 0, msg_size;
     char message[MAX_INPUT_LENGTH];
     struct sockaddr_in server;
     int port = atoi(argv[2]);
 
-    // Loop infinito
-    while(1)
+    // Zera o buffer das mensagens
+    bzero(message, MAX_INPUT_LENGTH);
+    // Aguarda a captação do teclado para encaminhar a mensagem ao servidor
+    printf("Entre com a mensagem: ");
+    fgets(message, MAX_INPUT_LENGTH, stdin);
+      
+    msg_size = strlen(message);
+    
+    // Remove quebra de linha da msg
+	if (message[--msg_size] == '\n')
+	    message[msg_size] = '\0';
+
+    // Abre comunicação com o servidor utilizando protocolo TCP/IP e executa tratativa de sucesso
+    if((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        // Zera o buffer das mensagens
-        bzero(message, MAX_INPUT_LENGTH);
-        // Aguarda a captação do teclado para encaminhar a mensagem ao servidor
-        printf("Entre com a mensagem: ");
-        fgets(message, MAX_INPUT_LENGTH, stdin);
-
-	    // Remove quebra de linha da msg
-	    int msg_size = strlen(message);
-	    if (message[--msg_size] == '\n')
-		    message[msg_size] = '\0';
-
-        // Abre comunicação com o servidor utilizando protocolo TCP/IP e executa tratativa de sucesso
-        if((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-        {
-            perror("Nao foi possivel criar o socket!\n");
-            exit(1);
-        }
-
-        // Configurações do servidor
-        server.sin_family = AF_INET;                    // Família do endereço, no caso IPv4
-        server.sin_port = htons(port);                  // Configuração da porta
-        server.sin_addr.s_addr = inet_addr(argv[1]);    // Configuração do IP
-        bzero(&(server.sin_zero), 8);                   //
-
-        // Loop para tentativas de conexão ao servidor
-        for(int i = 0; i < 5; i++)
-        {
-            if(connect(fd, (struct sockaddr *)&server, sizeof(server)) == 0)
-                break;
-
-            // Tratativa de falha para caso a conexão nao seja estabelecida corretamente   
-            else
-            {
-                perror("Nao foi possivel conectar ao servidor!\n");
-                fflush(stdin);
-                exit(1);
-            }   
-        }
-
-        // Tratativa de falha em caso da mensagem não ser encaminhada corretamente ao servidor
-        if(send(fd, message, MAX_INPUT_LENGTH, 0) < 0)
-        {
-            printf("Mensagem nao foi enviada!\n");
-            exit(1);
-        }
-
-        // Limpa a variável que recebe o número de bytes da mensagem e limpa o vetor message
-        num_bytes = 0;
-        bzero(message, MAX_INPUT_LENGTH);
-        // Loop de aguardo de chegada da mensagem
-        while(num_bytes <= 0)
-            num_bytes = recv(fd, message, MAX_INPUT_LENGTH, 0);
-
-        // Imprime a mensagem alterada pelo servidor
-        printf("Servidor: %s\n", message);
-            
+        perror("Nao foi possivel criar o socket!\n");
+        exit(1);
     }
 
+    // Configurações do servidor
+    server.sin_family = AF_INET;                    // Família do endereço, no caso IPv4
+    server.sin_port = htons(port);                  // Configuração da porta
+    server.sin_addr.s_addr = inet_addr(argv[1]);    // Configuração do IP
+    bzero(&(server.sin_zero), 8);                   //
+
+    // Loop para tentativas de conexão ao servidor
+    for(int i = 0; i < 5; i++)
+    {
+        if(connect(fd, (struct sockaddr *)&server, sizeof(server)) == 0)
+            break;
+
+        // Tratativa de falha para caso a conexão nao seja estabelecida corretamente   
+        else
+        {
+            perror("Nao foi possivel conectar ao servidor!\n");
+            fflush(stdin);
+            exit(1);
+        }   
+    }
+
+    // Tratativa de falha em caso da mensagem não ser encaminhada corretamente ao servidor
+    if(send(fd, message, MAX_INPUT_LENGTH, 0) < 0)
+    {
+        printf("Mensagem nao foi enviada!\n");
+        exit(1);
+    }
+
+    // Limpa a variável que recebe o número de bytes da mensagem e limpa o vetor message
+    num_bytes = 0;
+    bzero(message, MAX_INPUT_LENGTH);
+    // Loop de aguardo de chegada da mensagem
+    while(num_bytes <= 0)
+        num_bytes = recv(fd, message, MAX_INPUT_LENGTH, 0);
+
+    // Imprime a mensagem alterada pelo servidor
+    printf("Servidor: %s\n", message);
+
     close(fd);
+    return 0;
 }
